1ccfcd39e02305243fd98a2cbd650b39ba286b5f
abdelheq <hakoumokhtari6@gmail.com>
2024-02-19 16:09:47 +0100

    Furthest Building (1642) -Min Heap Solution- C++

--- C++/Meduim/Furthest Building/Furthest Building.cpp
+++ C++/Meduim/Furthest Building/Furthest Building.cpp
@@ -4,30 +4,32 @@
 #include <unordered_set>
 #include <map>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-	for (int i = 0; i < heights.size()-1; i++) {
-		if (heights[i + 1] <= heights[i]) {
+	priority_queue<int, vector<int>, greater<int> > steps;
+	for (size_t i = 0; i < heights.size()-1; i++) {
+		if (heights[i] >= heights[i + 1]) {
 			continue;
 		}
-		else if ((bricks < heights[i + 1] - heights[i]) && ladders == 0) {
-			return i;
-		}
-		else if((heights[i + 1] - heights[i]) <= bricks){
-			bricks -= heights[i + 1] - heights[i];
-		}
-		else if(ladders != 0){
-			ladders -= 1;
+		steps.push(heights[i + 1] - heights[i]);
+		if (steps.size() > ladders) {
+			bricks -= steps.top();
+			steps.pop();
+			if (bricks < 0) {
+				return i;
+			}
 		}
 	}
+	
 	return heights.size()-1;
 }
 
 int main() {
-	vector<int> nums = { 4,12,2,7,3,18,20,3,19 };
-	int bricks = 10;
-	int ladders = 2;
+	vector<int> nums = { 4,2,7,6,9,14,12 };
+	int bricks = 5;
+	int ladders = 1;
 	int output = furthestBuilding(nums, bricks,ladders);
 	cout << output;
 }	
