b398f54394fd6bf1efca73b3c1d2dbc5be3524ba
AbdelheqMokhtari <hakoumokhtari6@gmail.com>
2025-01-01 19:41:51 +0100

    Solve 217 Time O(n) and Space O(n) Complixity

--- Python/Easy/217. Contains Duplicate/Contains Duplicate.py
+++ Python/Easy/217. Contains Duplicate/Contains Duplicate.py
@@ -10,6 +10,7 @@ def containsDuplicate( nums: list[int]) -> bool:
 
 if __name__ ==  "__main__" :
 
+    # Test Cases
     nums = [1, 2, 3, 1]
     print(f'does the list {nums} contain duplicated ? {containsDuplicate(nums)}') # Output: True
     nums1 = [1, 2, 3, 4]
@@ -22,3 +23,12 @@ if __name__ ==  "__main__" :
     print(f'does the list {nums4} contain duplicated ? {containsDuplicate(nums4)}') # Output: True
     nums5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
     print(f'does the list {nums5} contain duplicated ? {containsDuplicate(nums5)}') # Output: True
+    nums6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    print(f'does the list {nums6} contain duplicated ? {containsDuplicate(nums6)}') # Output: True
+    nums7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10]
+    print(f'does the list {nums7} contain duplicated ? {containsDuplicate(nums7)}') # Output: True
+    nums8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10]
+    print(f'does the list {nums8} contain duplicated ? {containsDuplicate(nums8)}') # Output: True
+
+    nums9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    print(f'does the list {nums9} contain duplicated ? {containsDuplicate(nums9)}') # Output: False
