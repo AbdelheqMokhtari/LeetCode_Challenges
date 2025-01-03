8a31d77f7af0497d3cd0c72ace1c4a0c2bd85f75
AbdelheqMokhtari <hakoumokhtari6@gmail.com>
2025-01-03 11:12:33 +0100

    Two Pass Hash table Python Two Sum

--- Python/Easy/1.Two Sum/Two Sum.py
+++ Python/Easy/1.Two Sum/Two Sum.py
@@ -1,9 +1,21 @@
+# Brute force solution
 def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)-1):
         for j in range(i+1,len(nums)):
             if nums[i] + nums[j] == target:
                 return [i,j]
 
+# Two pass hash table solution
+def twoSumhash(nums: list[int], target: int) -> list[int]:
+    hash_table = {}
+    for i in range(len(nums)):
+        hash_table[nums[i]] = i
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hash_table and hash_table[complement] != i:
+            return [i, hash_table[complement]]
+
+# Test cases
 if __name__ == '__main__':
     nums = [2,7,11,15]
     target = 9
