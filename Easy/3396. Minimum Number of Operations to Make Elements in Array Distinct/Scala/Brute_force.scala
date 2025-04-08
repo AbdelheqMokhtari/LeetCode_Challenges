8d60109f112d62596ea22c4e28d2de0a75ced21a
AbdelheqMokhtari <hakoumokhtari6@gmail.com>
2025-04-08 13:00:39 +0200

    Brute force solution for 3396 challenge using Scala

--- Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct/Scala/Brute_force.scala
+++ Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct/Scala/Brute_force.scala
@@ -15,16 +15,16 @@ object Solution {
     }
 
         def main(args: Array[String]): Unit = {
-        val nums1 = Array(1, 2, 3, 4, 2, 3, 3, 5, 7)
-        val nums2 = Array(4, 5, 6, 4, 4)
-        val nums3 = Array(5, 4, 3, 2, 1)
-        val nums4 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
-        val nums5 = Array(6, 7, 8, 9)
+            val nums1 = Array(1, 2, 3, 4, 2, 3, 3, 5, 7)
+            val nums2 = Array(4, 5, 6, 4, 4)
+            val nums3 = Array(5, 4, 3, 2, 1)
+            val nums4 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            val nums5 = Array(6, 7, 8, 9)
 
-        println(s"Result for nums1: ${minimumOperations(nums1)}")  // Example 1
-        println(s"Result for nums2: ${minimumOperations(nums2)}")  // Example 2
-        println(s"Result for nums3: ${minimumOperations(nums3)}")  // Example 3
-        println(s"Result for nums4: ${minimumOperations(nums4)}")  // Example 4
-        println(s"Result for nums5: ${minimumOperations(nums4)}")  // Example 4
+            println(s"Result for nums1: ${minimumOperations(nums1)}")  // Example 1
+            println(s"Result for nums2: ${minimumOperations(nums2)}")  // Example 2
+            println(s"Result for nums3: ${minimumOperations(nums3)}")  // Example 3
+            println(s"Result for nums4: ${minimumOperations(nums4)}")  // Example 4
+            println(s"Result for nums5: ${minimumOperations(nums4)}")  // Example 4
     }
 }
